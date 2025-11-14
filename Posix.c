892f8ebb9ad026ac6fa6534a718635beb759fe83
Costa <joaquincostapi@gmail.com>
2025-11-14 14:14:52 -0300

    docs (Posix.c) se implemento posible correcion de LectoresEscritores

--- Posix.c
+++ Posix.c
@@ -1,126 +1,95 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <semaphore.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include <time.h>
 
 #define CANT_LECTORES 100
 #define CANT_ESCRITORES 5
-#define SIZE_CARTEL 20
+#define TOTAL_HILOS 105
 
-// struct vuelo, ref de IA prompt:"para crear un struct en c es similar a c++? como lo creo?"
-typedef struct {
-    int hora;
-    char destination[20];
-    char flight[8];
-    int gate;
-    char remarks[10];
-} vuelo;
-
-// Variables compartidas
-vuelo* cartel[SIZE_CARTEL];
-char* tipos[] = {"On Time", "Delayed", "Cancelled"};
-
-// Sincronizacion
-sem_t sem_escritor;
-pthread_mutex_t mutex_lectores;
+sem_t turno;
+sem_t rw_mutex;
+pthread_mutex_t mutex;
 int lectores_activos = 0;
 
-// Funcion para agregar vuelos
-void* agregarVuelo(vuelo** cartel, int cant) {
-    for (int i = 0; i < cant; i++) {
-        vuelo* v = (vuelo*)malloc(sizeof(vuelo));
-        v->hora = (rand() % 24) * 100 + (rand() % 60);
-        sprintf(v->destination, "City%d", rand() % 100);
-        sprintf(v->flight, "FL%03d", rand() % 1000);
-        v->gate = rand() % 20 + 1;
-        strcpy(v->remarks, tipos[i % 3]);
-        cartel[i] = v;
-    }
-    return NULL;
-}
+void* lector(void* arg) {
+    int id = *(int*)arg;
 
-// Funcion para modificar vuelos
-void* modificarVuelo(vuelo** cartel, int cant) {
-    for (int i = 0; i < cant; i++) {
-        int index = rand() % SIZE_CARTEL;
-        cartel[index]->hora = (rand() % 24) * 100 + (rand() % 60);
-        cartel[index]->gate = rand() % 20 + 1;
-        strcpy(cartel[index]->remarks, tipos[rand() % 3]);
-    }
-    return NULL;
-}
-
-// Funcion lector
-void* lector(void* x) {
-    int id = *(int*)x;
-
-    pthread_mutex_lock(&mutex_lectores);
+    sem_wait(&turno);
+    pthread_mutex_lock(&mutex);
     lectores_activos++;
     if (lectores_activos == 1) {
-        sem_wait(&sem_escritor);
+        sem_wait(&rw_mutex);
     }
-    pthread_mutex_unlock(&mutex_lectores);
+    pthread_mutex_unlock(&mutex);
+    sem_post(&turno);
 
-    printf("Pasajero %d esta mirando el cartel\n", id);
-    sleep(1); // Simula lectura
+    printf("Pasajero %d esta leyendo el cartel\n", id);
+    sleep(rand() % 3 + 1);
 
-    pthread_mutex_lock(&mutex_lectores);
+    pthread_mutex_lock(&mutex);
     lectores_activos--;
     if (lectores_activos == 0) {
-        sem_post(&sem_escritor);
+        sem_post(&rw_mutex);
     }
-    pthread_mutex_unlock(&mutex_lectores);
+    pthread_mutex_unlock(&mutex);
 
     return NULL;
 }
 
-// Funcion escritor
-void* escritor(void* x) {
-    int id = *(int*)x;
+void* escritor(void* arg) {
+    int id = *(int*)arg;
 
-    sem_wait(&sem_escritor);
+    sem_wait(&turno);
+    sem_wait(&rw_mutex);
+    sem_post(&turno);
+
     printf("Oficinista %d esta modificando el cartel\n", id);
-    modificarVuelo(cartel, rand() % 5 + 1);
-    sleep(1); // Simula escritura
-    sem_post(&sem_escritor);
+    sleep(rand() % 4 + 1);
+
+    sem_post(&rw_mutex);
 
     return NULL;
 }
 
 int main() {
     srand(time(NULL));
-    agregarVuelo(cartel, SIZE_CARTEL);
 
-    pthread_t lectores[CANT_LECTORES], escritores[CANT_ESCRITORES];
-    int id_lectores[CANT_LECTORES], id_escritores[CANT_ESCRITORES];
+    pthread_t hilos[TOTAL_HILOS];
+    int ids[TOTAL_HILOS];
 
-    pthread_mutex_init(&mutex_lectores, NULL);
-    sem_init(&sem_escritor, 0, 1);
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&turno, 0, 1);
+    sem_init(&rw_mutex, 0, 1);
 
-    for (int i = 1; i <= CANT_LECTORES; i++) {
-        id_lectores[i] = i;
-        pthread_create(&lectores[i], NULL, lector, &id_lectores[i]);
+    char roles[TOTAL_HILOS];
+    for (int i = 0; i < CANT_LECTORES; i++) roles[i] = 'L';
+    for (int i = CANT_LECTORES; i < TOTAL_HILOS; i++) roles[i] = 'E';
+
+    for (int i = TOTAL_HILOS - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        char temp = roles[i];
+        roles[i] = roles[j];
+        roles[j] = temp;
     }
 
-    for (int i = 1; i <= CANT_ESCRITORES; i++) {
-        id_escritores[i] = i;
-        pthread_create(&escritores[i], NULL, escritor, &id_escritores[i]);
+    int lector_id = 1, escritor_id = 1;
+    for (int i = 0; i < TOTAL_HILOS; i++) {
+        if (roles[i] == 'L') {
+            ids[i] = lector_id++;
+            pthread_create(&hilos[i], NULL, lector, &ids[i]);
+        } else {
+            ids[i] = escritor_id++;
+            pthread_create(&hilos[i], NULL, escritor, &ids[i]);
+        }
+        usleep(50000); // ! Agregue este sleep para que no se mezclen los hilos incorrectamente y respetar un orden
     }
 
-    for (int i = 1; i <= CANT_LECTORES; i++) {
-        pthread_join(lectores[i], NULL);
-    }
-
-    for (int i = 1; i <= CANT_ESCRITORES; i++) {
-        pthread_join(escritores[i], NULL);
+    for (int i = 0; i < TOTAL_HILOS; i++) {
+        pthread_join(hilos[i], NULL);
     }
 
     return 0;
 }
-
-//! En la Bibliografia agregar:
-//! Link de consulta por escritores[i] para darles nombre id
-//! NOTA: se uso #define, en vez de una variable para no generar errores con numeros fijos
