3935a7831efa74370e56a5ace4327d0392bf0c07
ezequiel <ezequielriv20@gmail.com>
2025-12-08 20:22:04 -0300

    version exitosa 2

--- Ej2/Posix.c
+++ Ej2/Posix.c
@@ -1,69 +1,115 @@
-
 #include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
-#define CANT_PASAJEROS 100
-#define CANT_OFICINISTAS 15
+#define CANT_LECTORES 100
+#define CANT_ESCRITORES 5
+#define MODS_POR_ESCRITOR 3
+
+#include <semaphore.h>
 
 sem_t wrt;
-sem_t mutex;
-int rdr = 0;
+sem_t mutex_lectores;
+sem_t mutex_escritores;
 
-void* Lector(void* arg) {
+int cant_lectores = 0;
+int cant_escritores = 0;
+
+void* lector(void* arg) {
     int id = *(int*)arg;
-    sem_wait(&mutex);
-    rdr++;
-    if (rdr == 1) sem_wait(&wrt);
-    sem_post(&mutex);
-    printf("Pasajero %d está viendo el cartel\n", id);
-    sem_wait(&mutex);
-    rdr--;
-    if (rdr == 0) sem_post(&wrt);
-    sem_post(&mutex);
+
+    sem_wait(&mutex_escritores);
+    while (cant_escritores > 0) {
+        sem_post(&mutex_escritores);
+        sem_wait(&mutex_escritores);
+    }
+    sem_post(&mutex_escritores);
+
+    sem_wait(&mutex_lectores);
+    cant_lectores++;
+    if (cant_lectores == 1) {
+        sem_wait(&wrt);
+    }
+    sem_post(&mutex_lectores);
+
+    printf("Pasajero %d esta mirando el cartel\n", id);
+    sleep(rand() % 3);
+    sem_wait(&mutex_lectores);
+    cant_lectores--;
+    if (cant_lectores == 0) {
+        sem_post(&wrt);
+    }
+    sem_post(&mutex_lectores);
+
     return NULL;
 }
 
-void* Escritor(void* arg) {
+void* escritor(void* arg) {
     int id = *(int*)arg;
+
+    sem_wait(&mutex_escritores);
+    cant_escritores++;
+    sem_post(&mutex_escritores);
+
     sem_wait(&wrt);
-    printf("Oficinista %d está escribiendo\n", id);
+
+    printf("Oficinista %d esta modificando el cartel\n", id);
+    sleep(rand() % 5);
     sem_post(&wrt);
+
+    sem_wait(&mutex_escritores);
+    cant_escritores--;
+    sem_post(&mutex_escritores);
+
     return NULL;
 }
 
-int main(void) {
+int main() {
+    srand(time(NULL));
+
     sem_init(&wrt, 0, 1);
-    sem_init(&mutex, 0, 1);
+    sem_init(&mutex_lectores, 0, 1);
+    sem_init(&mutex_escritores, 0, 1);
 
-    pthread_t hLectores[CANT_PASAJEROS];
-    pthread_t hEscritores[CANT_OFICINISTAS];
-    int idL[CANT_PASAJEROS];
-    int idE[CANT_OFICINISTAS];
+    int total_escrituras = CANT_ESCRITORES * MODS_POR_ESCRITOR;
+    int total_hilos = CANT_LECTORES + total_escrituras;
 
-    int idxL = 0;
-    for (int j = 0; j < CANT_OFICINISTAS; j++) {
-        idE[j] = j + 1;
-        pthread_create(&hEscritores[j], NULL, Escritor, &idE[j]);
-    }
-    for (int k = 0; k < CANT_PASAJEROS; k++) {
-            idL[idxL] = idxL + 1;
-            pthread_create(&hLectores[idxL], NULL, Lector, &idL[idxL]);
-            idxL++;
-    }
+    pthread_t hilos[total_hilos];
+    char roles[total_hilos];
+    int ids_lectores[CANT_LECTORES];
+    int ids_escritores[total_escrituras];
 
-    while (idxL < CANT_PASAJEROS) {
-        idL[idxL] = idxL + 1;
-        pthread_create(&hLectores[idxL], NULL, Lector, &idL[idxL]);
-        idxL++;
+    for (int i = 0; i < CANT_LECTORES; i++) roles[i] = 'L';
+    for (int i = CANT_LECTORES; i < total_hilos; i++) roles[i] = 'E';
+
+    for (int i = total_hilos - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        char temp = roles[i];
+        roles[i] = roles[j];
+        roles[j] = temp;
     }
 
-    for (int j = 0; j < CANT_OFICINISTAS; j++) pthread_join(hEscritores[j], NULL);
-    for (int i = 0; i < CANT_PASAJEROS; i++) pthread_join(hLectores[i], NULL);
+    int lector_idx = 0;
+    int escritor_idx = 0;
 
-    sem_destroy(&wrt);
-    sem_destroy(&mutex);
+    for (int i = 0; i < total_hilos; i++) {
+        if (roles[i] == 'L') {
+            ids_lectores[lector_idx] = lector_idx + 1;
+            pthread_create(&hilos[i], NULL, lector, &ids_lectores[lector_idx]);
+            lector_idx++;
+        } else {
+            ids_escritores[escritor_idx] = (escritor_idx / MODS_POR_ESCRITOR) + 1;
+            pthread_create(&hilos[i], NULL, escritor, &ids_escritores[escritor_idx]);
+            escritor_idx++;
+        }
+        sleep(1);
+    }
 
-    printf("Fin: %d pasajeros y %d oficinistas.\n", CANT_PASAJEROS, CANT_OFICINISTAS);
+    for (int i = 0; i < total_hilos; i++) {
+        pthread_join(hilos[i], NULL);
+    }
+
     return 0;
 }
